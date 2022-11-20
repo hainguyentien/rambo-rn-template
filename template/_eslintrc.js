6dc25cb93b7d3e759449b4fa11c5293b52e4afa3
Rambo <hai.nguyentien.0209@gmail.com>
2022-11-20 18:14:11 +0700

    Update missing files for Android and iOS

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        semi: "off",
+        "@typescript-eslint/no-shadow": ["error"],
+        "@typescript-eslint/no-unused-vars": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "react-native/no-inline-styles": "off",
+        "no-shadow": "off",
+        "no-undef": "off",
       },
     },
   ],
